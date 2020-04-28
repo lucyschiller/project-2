febf0c313f10e619bb13f771f573140befc79adc
Lucy Schiller <lucilles@uoregon.edu>
2020-04-27 22:44:20 -0700

    Functions file

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,52 @@
 // jshint esversion: 6
 
+// initial function for all requirements
+let revWord = str => {
+  let result = "";
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+  return result;
+};
+
+// Req. 3a
+let revWords1 = sent => {
+  let array = sent.split(" ");
+  let result = " ";
+  for (var i = 0; i < array.length; i++) {
+    result = result + " " + revWord(array[i]);
+  }
+  return result;
+};
+console.log(revWords1("Hello there world"));
+
+// Req. 3b
+
+let revWords2 = sent => {
+  let array = sent.split(" ");
+  let result = "";
+  array.forEach(char => {
+    result = result + " " + revWord(char);
+  });
+
+  return result;
+};
+console.log(revWords2("Hello world"));
+
+// Req. 3c
+
+let revWords3 = sent => {
+  let array = sent.split(" ");
+  let result = "";
+  for (char of array) {
+    result = result + " " + revWord(char);
+  }
+
+  return result;
+};
+console.log(revWords3("Hello world"));
+
+/*
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
@@ -14,3 +61,4 @@ let containsDuplicates = s => {
   }
   return false;
 };
+*/
